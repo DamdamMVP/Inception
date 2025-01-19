495a93d222286892230c596f378a2aa2ad40ce35
Damien LEBRANCHU <dams.lascards@gmail.com>
2025-01-19 23:55:48 +0100

    +: sheet sheet

--- srcs/requirements/mariadb/config/entrypoint.sh
+++ srcs/requirements/mariadb/config/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Vérifie si la DB système MySQL existe déjà
+# MySQL init script
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo ">>> [Entrypoint] Initialisation de la base de données..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
@@ -9,7 +9,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
     pid="$!"
 
-    # Attendre que MySQL soit prêt
+    # Try a simple command to see if the server is ready
     for i in {30..0}; do
         if echo 'SELECT 1' | mysql &> /dev/null; then
             break
@@ -22,21 +22,23 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         exit 1
     fi
 
+    # Configure the user
     echo ">>> [Entrypoint] Configuration de la base utilisateur..."
     mysql <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
+    CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+    GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+    DELETE FROM mysql.user WHERE User='';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost');
 
-FLUSH PRIVILEGES;
+    FLUSH PRIVILEGES;
 EOF
 
+    # Stop the temporary server
     echo ">>> [Entrypoint] Arrêt du serveur provisoire..."
     if ! kill -s TERM "$pid" || ! wait "$pid"; then
         echo >&2 ">>> [Entrypoint] Échec lors de l'arrêt de MySQL après init."
@@ -45,6 +47,6 @@ EOF
     echo ">>> [Entrypoint] Initialisation terminée."
 fi
 
-# Enfin, on lance MariaDB "normalement" sur le port 3306
+# Launch MariaDB
 echo ">>> [Entrypoint] Lancement de MariaDB..."
 exec mysqld --user=mysql --console
